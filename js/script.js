354f63b84673614c4491bc6a9fbdbd2d34bbe75d
Taras <Taras66694@gmail.com>
2022-03-09 17:16:33 +0200

    new

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: false,
-        prevArrow:  '<button type="button" class="slick-prev"><img src="../img/chevron left solid.png"></img></button>',
-        nextArrow:  '<button type="button" class="slick-next"><img src="../img/chevron right solid.png"></img></button>',
+        prevArrow:  '<button type="button" class="slick-prev"><img src="img/chevron left solid.png"></img></button>',
+        nextArrow:  '<button type="button" class="slick-next"><img src="img/chevron right solid.png"></img></button>',
         responsive: [
             {
                 breakpoint: 768,
